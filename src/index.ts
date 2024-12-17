de66f35d5742ab56f7522257afd78fe9f14123d6
Jeff Eaton <jeff@eaton.fyi>
2024-12-17 12:08:13 -0600

    Exporting fun bits

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export * from './progress-formatter.js';
+export * from './status-tracker.js';
+export * from './status-emitter.js';
+export * from './formatters/progress.js';
+export * from './formatters/sparkline.js';
+export * from './formatters/dotline.js';
